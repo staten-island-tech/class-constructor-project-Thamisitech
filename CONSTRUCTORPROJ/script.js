642b55b5aea01b4a0e21c3861b34e99cee30c129
Thamisitech <50879028+Thamisitech@users.noreply.github.com>
2019-11-04 10:21:57 -0500

    Update script.js

--- CONSTRUCTORPROJ/script.js
+++ CONSTRUCTORPROJ/script.js
@@ -1,66 +1,65 @@
-class Album{
-    constructor(name, year, url) {
-        this.name = name;
-        this.year = year;
-        this.url = url;
-    }
+class Profile {
+  constructor(name, year, url) {
+    this.name = name;
+    this.year = year;
+    this.url = url;
+  }
 }
 
 class UI {
-    addAlbumToList(album) {
-        const display = document.querySelector('.display');
+  addProfileToList(profile) {
+    const display = document.querySelector(".display");
 
-        let html = '<div class="display-profile flex-row"><div class="display-name">%name%</div><div class="display-year">%year%</div><div class="display-pic"><img src="%url%"alt=""></div><div class="remove-profile">Remove President! X</div>'
-        let newHtml = html.replace('%name%', album.name);
-        newHtml = newHtml.replace('%year%', album.year);
-        newHtml = newHtml.replace('%url%', album.url);
-        document.querySelector('.display').insertAdjacentHTML('beforeend', newHtml);
+    let html =
+      '<div class="display-profile flex-row"><div class="display-name">%name%</div><div class="display-year">%year%</div><div class="display-pic"><img src="%url%"alt=""></div><div class="remove-profile">Remove President! X</div>';
+    let newHtml = html.replace("%name%", profile.name);
+    newHtml = newHtml.replace("%year%", profile.year);
+    newHtml = newHtml.replace("%url%", profile.url);
+    document.querySelector(".display").insertAdjacentHTML("beforeend", newHtml);
+  }
 
+  clearFields() {
+    document.getElementById("name").value = "";
+    document.getElementById("year").value = "";
+    document.getElementById("url").value = "";
+  }
 
+  deleteProfile(target) {
+    if (target.className === "remove-profile") {
+      target.parentElement.remove();
     }
-
-    clearFields(){
-        document.getElementById('name').value = '';
-        document.getElementById('year').value = '';
-        document.getElementById('url').value = '';
-    }
-
-    deleteAlbum(target){
-        if(target.className === 'remove-profile') {
-            target.parentElement.remove();
-        }
-    }
+  }
 }
 
-//Event Listener for Album
-document.getElementById('form').addEventListener('submit', function(e){
-    //Get Values
-    const name = document.getElementById('name').value;
-    const year = document.getElementById('year').value;
-    const url = document.getElementById('url').value;
+//Event Listener for Profile
+document.getElementById("form").addEventListener("submit", function(e) {
+  //Get Values
+  const name = document.getElementById("name").value;
+  const year = document.getElementById("year").value;
+  const url = document.getElementById("url").value;
 
-    //inmstantiate new album
-    const album = new Album(name, year, url);
-    //console.log(album);
+  //inmstantiate new profile
+  const profile = new Profile(name, year, url);
+  //console.log(profile);
 
-    const ui = new UI();
-    console.log(ui);
+  const ui = new UI();
+  console.log(ui);
 
-    ui.addAlbumToList(album);
+  ui.addProfileToList(profile);
 
-    ui.clearFields();
+  ui.clearFields();
 
-    e.preventDefault();
+  e.preventDefault();
 });
 
 //delete
 
-document.querySelector('.display').addEventListener('click', function(e){
-    const ui = new UI();
-    
-    ui.deleteAlbum(e.target);
+document.querySelector(".display").addEventListener("click", function(e) {
+  const ui = new UI();
 
-    ui.clearFields();
+  ui.deleteProfile(e.target);
 
-    e.preventDefault();
-})
+  ui.clearFields();
+
+  e.preventDefault();
+});
